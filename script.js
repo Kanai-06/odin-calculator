bd5caecb697622a476c35b6ff49606009c36616b
Kanai-06 <153616932+Kanai-06@users.noreply.github.com>
2024-09-11 16:10:27 +0200

    Add operations functin and exeptions

---
+++ script.js
@@ -0,0 +1,43 @@
+const add = function(a, b) {
+	if(typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
+    
+    return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const divide = function(a, b) {
+    if(b === 0 || typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
+
+	return Math.round(((a / b) + Number.EPSILON) * 1000000) / 1000000;
+};
+
+const multiply = function(a, b) {
+    if(typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
+
+    return a * b;
+};
+
+const power = function(a, b) {
+    if(typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
+
+	return Math.round((Math.pow(a, b) + Number.EPSILON) * 1000000) / 1000000;
+};
+
+const factorial = function(a) {
+	if(a < 0 || typeof(a) !== "number") return "ERROR";
+
+    if(a === 0){
+        return 1;
+    }else {
+        return factorial(a - 1) * a;
+    }
+};
+
+const percent = function(a, b){
+    if(!(b >= 0 || b <= 100) || typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
+    
+    return Math.round(((a * (b / 100)) + Number.EPSILON) * 1000000) / 1000000;
+}
